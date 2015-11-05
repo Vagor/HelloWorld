f4941bdf3435c421a1bc65e7822f598176b6a491
Vagor <vagor.tao@gmail.com>
2015-11-05 19:08:00 +0800

    更新

--- files/script.js
+++ files/script.js
@@ -1,3 +1,8 @@
-$(".game_start_footer_btn").click(function(){
-	$(this).attr("src","./images/mainpage-press.png")
-})
+// game_start button
+$('.game_start_footer_btn').bind("touchstart",function() {
+	$(this).attr("src","./images/mainpage-press.png");
+});
+$('.game_start_footer_btn').bind("touchend",function() {
+	$(this).attr("src","./images/mainpage-normal.png");
+});
+
